8bfc73dc770be5a99fcc08dfda1b44d5de4afec8
zurukumo <zurukumo@gmail.com>
2024-07-25 07:20:06 +0900

    feat: 一旦完成

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '../../assets/styles/tailwind.css';
 import Popup from './Popup';
-import './index.css';
 
 const container = document.getElementById('app-container');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!);
 root.render(<Popup />);
